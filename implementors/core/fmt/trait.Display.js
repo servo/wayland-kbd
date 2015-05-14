e3ea3b4a261ebf2a30433e50389d2004e1883aa9
Victor Berger <victor.berger@m4x.org>
2015-05-14 20:50:00 +0200

    Update docs for 7d753e108ebc3bb6cca7c541343ffeaf38573288
    Fix callback API before publishing.

--- implementors/core/fmt/trait.Display.js
+++ implementors/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['wayland_client'] = [];implementors['mmap'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='mmap/enum.MapError.html' title='mmap::MapError'>MapError</a>",];implementors['wayland_kbd'] = [];implementors['wayland_kbd'] = [];implementors['wayland_kbd'] = [];implementors['wayland_kbd'] = [];
+implementors['wayland_client'] = [];implementors['mmap'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Display.html' title='core::fmt::Display'>Display</a> for <a class='enum' href='mmap/enum.MapError.html' title='mmap::MapError'>MapError</a>",];implementors['wayland_kbd'] = [];implementors['wayland_kbd'] = [];implementors['wayland_kbd'] = [];implementors['wayland_kbd'] = [];implementors['wayland_kbd'] = [];implementors['wayland_kbd'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
