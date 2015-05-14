4b2b3ceb1b05deeeb34f9d3fec9228167d24db9a
Victor Berger <victor.berger@m4x.org>
2015-05-14 20:41:28 +0200

    Update docs for 961b657f3f0981f1df04400010a8720d13c951d6
    Update README.

--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rand'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='rand/reseeding/struct.ReseedWithDefault.html' title='rand::reseeding::ReseedWithDefault'>ReseedWithDefault</a>",];implementors['wayland_client'] = [];implementors['wayland_kbd'] = [];implementors['wayland_kbd'] = [];implementors['wayland_kbd'] = [];
+implementors['rand'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='rand/reseeding/struct.ReseedWithDefault.html' title='rand::reseeding::ReseedWithDefault'>ReseedWithDefault</a>",];implementors['wayland_client'] = [];implementors['wayland_kbd'] = [];implementors['wayland_kbd'] = [];implementors['wayland_kbd'] = [];implementors['wayland_kbd'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
